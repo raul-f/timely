e504828f1d3f72e56de903e045e7015fc2b3fb31
raul-f <raulf.dev@gmail.com>
2019-01-11 17:12:38 -0300

    the project has been reformed and all functionality is in place

--- server.js
+++ server.js
@@ -18,15 +18,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get('/api/timestamp', (req, res) => {
+	if ((new Date(req.query.date_string)).getTime()) {
+		req.date = new Date(req.query.date_string);
+		console.log(`The value of 'date_string' provided, ${req.query.date_string}, is valid, and the associated date is ${req.date}.`);
+	} else if ((new Date(parseInt(req.query.date_string))).getTime()) {
+		req.date = new Date(parseInt(req.query.date_string));
+		console.log(`The value of 'date_string' provided, ${req.query.date_string}, is valid, the parsed value is ${parseInt(req.query.date_string)} and the associated date is ${req.date}.`);
+	} else if (req.query.date_string) {
+		req.date = new Date(req.query.date_string);
+		console.log(req.date);
+		console.log(`The value of 'date_string' provided, ${req.query.date_string}, is not valid.`);
+	} else {
+		req.date = new Date();
+		console.log(`No value of 'date_string' has been provided, so the current date of ${req.date} will be used.`);
+	}
+	res.json({unix: (req.date).getTime(), utc: (req.date).toUTCString()});
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
